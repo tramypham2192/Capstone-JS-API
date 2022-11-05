8f0ee16201b6b0cdf5e93798df844ee00041c87a
my <tramypham2192@gmail.com>
2022-11-04 23:42:14 -0700

    final

---
+++ models/UserRegister.js
@@ -0,0 +1,10 @@
+//Tao ra format cho cac object User 
+function User(){
+    this.email = '';
+    this.password = '';
+    this.name = '';
+    this.phone = '';
+    this.gender = '';
+}
+
+export default User;
